f9b79a4f0e8acd8da45331be65f8823c7a22bebf
Ilhana Osmanković <iosmankovi2@etf.unsa.ba>
2025-04-11 11:17:00 +0200

    Configuration is done

--- activity-management/activity-management/target/generated-sources/annotations/com/example/activitymanagement/mapper/ActivityVolunteerMapperImpl.java
+++ activity-management/activity-management/target/generated-sources/annotations/com/example/activitymanagement/mapper/ActivityVolunteerMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-04-11T09:46:41+0200",
+    date = "2025-04-11T11:16:31+0200",
     comments = "version: 1.5.2.Final, compiler: Eclipse JDT (IDE) 3.42.0.z20250331-1358, environment: Java 21.0.6 (Eclipse Adoptium)"
 )
 @Component
