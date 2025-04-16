99703945893d326f704b061e1ded824cdcc0e787
Anesa <akrslak17@gmail.com>
2025-04-16 11:09:07 +0200

    Eureka-fix commit.

--- feedback-service/feedback-service/src/main/java/com/example/App.java
+++ feedback-service/feedback-service/src/main/java/com/example/App.java
@@ -2,10 +2,21 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    @LoadBalanced 
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
