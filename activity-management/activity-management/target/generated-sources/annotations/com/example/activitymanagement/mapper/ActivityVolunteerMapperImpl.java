c0b296199cf57e933955c76d054fd4f5c09af976
Anesa <akrslak17@gmail.com>
2025-04-16 10:05:04 +0200

    Eureka

--- activity-management/activity-management/target/generated-sources/annotations/com/example/activitymanagement/mapper/ActivityVolunteerMapperImpl.java
+++ activity-management/activity-management/target/generated-sources/annotations/com/example/activitymanagement/mapper/ActivityVolunteerMapperImpl.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-04-04T19:33:20+0200",
-    comments = "version: 1.5.2.Final, compiler: javac, environment: Java 23.0.2 (Oracle Corporation)"
+    date = "2025-04-16T09:56:08+0200",
+    comments = "version: 1.5.2.Final, compiler: Eclipse JDT (IDE) 3.42.0.z20250331-1358, environment: Java 21.0.6 (Eclipse Adoptium)"
 )
 @Component
 public class ActivityVolunteerMapperImpl implements ActivityVolunteerMapper {
